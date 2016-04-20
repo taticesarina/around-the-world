5c68b8dbb75c2a830921eeaaf71ed0290d627505
taticesarina <tatiana.infante@mihsnyc.org>
2016-04-20 13:09:46 +0000

    countries

---
+++ js.js
@@ -0,0 +1,64 @@
+var countries={
+    Ecuador:{
+        capital:"Quito",
+        animal:"Condor",
+        language:"Spanish",
+        population:"16 468 795"
+    },
+        
+            "Dominican Republic":{
+        capital:"Santo Domingo",
+        animal:"Palmchat",
+        language:"Spanish",
+        population:"10 872 757"
+            },
+            
+            France:{
+        capital:"Paris",
+        animal:"Gallic rooster",
+        language:"French",
+        population:"64,668,129"
+                
+            },
+        
+            Argentina:{
+        capital:"Buenos Aires",
+        animal:"Rufous hornero",
+        language:"Spanish",
+        population:"42 900 514"
+                
+            },
+        
+        
+        Egypt:{
+        capital:"Cairo",
+        animal:"Steppe eagle",
+        language:"Arabic",
+        population:"42 900 514"
+            
+        },
+        
+           Venezuela :{
+        capital:"Caracas",
+        animal:"Icterus icterus",
+        language:"Spanish",
+        population:"31 910 775"
+                
+            },
+        
+            Brazil:{
+        capital:"Brasília",
+        animal:"Macaw",
+        language:"Portuguese",
+        population:"208 476 938"
+                
+            }
+}
+        
+        
+        
+$(document).ready(function(){
+   $('button').click(function () {
+    $("bleh").val();
+});
+});
